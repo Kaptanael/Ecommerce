96ee17f5fe8fac4acd497feae04e86254b0c90bd
Md. Atikur Zaman <kaptan.cse@outlook.com>
2020-01-29 17:52:01 +0600

    add login

--- Ecommerce.Spa/ClientApp/src/app/admin/dashboard/dashboard.module.ts
+++ Ecommerce.Spa/ClientApp/src/app/admin/dashboard/dashboard.module.ts
@@ -1,12 +1,11 @@
-import { RouterModule } from "@angular/router";
-import { CoreModule } from "./../../core/core.module";
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { CoreModule } from "./../../core/core.module";
+
 import { DashboardComponent } from "./dashboard.component";
 import { DashboardRoutingModule } from "./dashboard.route";
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, RouterModule, CoreModule, DashboardRoutingModule]
+  imports: [CoreModule, DashboardRoutingModule]
 })
 export class DashboardModule {}
--- Ecommerce.Spa/ClientApp/src/app/app.module.ts
+++ Ecommerce.Spa/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,11 +9,15 @@ import { AppComponent } from "./app.component";
 import { HomeComponent } from "./admin/home/home.component";
 import { NotFoundComponent } from "./core/not-found/not-found.component";
 import { DashboardModule } from './admin/dashboard/dashboard.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, NotFoundComponent],
-    imports: [BrowserModule, HttpClientModule, CommonModule, RouterModule, AppRoutingModule, DashboardModule],
+    imports: [BrowserModule,        
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        DashboardModule],
     providers: [],
     bootstrap: [AppComponent]
 })
